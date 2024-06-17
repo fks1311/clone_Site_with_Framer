74b7bb489afa9ca0ef5615f3d6881f691b4ab6c9
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2024-06-17 10:29:00 +0900

    라우터 및 스타일 세팅

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Reset } from "styled-reset";
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Reset />
+    <RouterProvider>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
