ba68367ad32384aa9f210a55dbefb0c38eb23df8
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2024-06-21 11:01:47 +0900

    zoom > 이미지 삽입

--- src/products/zoom/components/ZoomHome.jsx
+++ src/products/zoom/components/ZoomHome.jsx
@@ -1,18 +1,57 @@
+import { Fragment } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { styled } from "styled-components";
 
+import img1 from "../assets/1.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+import img5 from "../assets/5.jpg";
+import img6 from "../assets/6.jpg";
+import img7 from "../assets/7.jpg";
+import img8 from "../assets/8.jpg";
+import img9 from "../assets/9.jpg";
+import img10 from "../assets/10.jpg";
+import img11 from "../assets/11.jpg";
+import img12 from "../assets/12.jpg";
+import img13 from "../assets/13.jpg";
+import img14 from "../assets/14.jpg";
+
 export default function ZoomHome() {
-  const arr = Array.from(Array(15), (_, index) => index);
+  const img = [
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    "",
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+  ];
   const { scrollYProgress } = useScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [6, 1]);
 
   return (
     <ViewContainer>
       <Container className="zoom-home" style={{ scale }}>
-        {arr.map((data, idx) => (
-          <Card key={idx} center={idx}>
-            {data}
-          </Card>
+        {img.map((data, idx) => (
+          <Fragment key={idx}>
+            {idx === 7 ? (
+              <Card>
+                Zoom In - Out Scroll Animation
+                <strong>scroll ↓</strong>
+              </Card>
+            ) : (
+              <ImgCard center={idx} img={data} />
+            )}
+          </Fragment>
         ))}
       </Container>
     </ViewContainer>
@@ -20,20 +59,37 @@ export default function ZoomHome() {
 }
 
 const ViewContainer = styled.div`
-  height: 300vh;
+  height: 400vh;
 `;
+
 const Container = styled(motion.div)`
   position: fixed;
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
-`;
-const Card = styled.div`
-  height: ${window.innerHeight / 3}px;
-  border: 1px solid black;
-  background-color: ${({ center }) => center === 7 && `#E0A75E`};
+  grid-template-columns: repeat(2, 1fr) 475px repeat(2, 1fr);
 `;
 
+const ImgCard = styled.div`
+  height: ${window.innerHeight / 3}px;
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Card = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  background-color: #80c4e9;
+  strong {
+    position: absolute;
+    bottom: 5rem;
+    color: #ff7f3e;
+  }
+`;
 /**
  * ViewContainer : scrollY 위한 높이
  * Container : { position:fixed; width: 100%; } 설정해야 중앙을 기준으로 zoom out
